3c239739ba41f36cd596bc1bef9a07eb5133b7df
athul <rpalab009@gmail.com>
2025-07-31 12:11:07 +0530

    project created

---
+++ server/student/views.py
@@ -0,0 +1,30 @@
+# students/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/student_list.html', {'students': students})
+
+def student_create(request):
+    form = StudentForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('student_list')
+    return render(request, 'students/student_form.html', {'form': form})
+
+def student_update(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    form = StudentForm(request.POST or None, request.FILES or None, instance=student)
+    if form.is_valid():
+        form.save()
+        return redirect('student_list')
+    return render(request, 'students/student_form.html', {'form': form})
+
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student_list')
+    return render(request, 'students/student_confirm_delete.html', {'student': student})
